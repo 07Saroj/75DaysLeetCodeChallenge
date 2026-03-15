4223705c2ed6b6443b548b797069eb70e60e8760
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-03-15 15:43:46 +0530

    Time: 6 ms (80.8%), Space: 67.1 MB (81.69%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,20 +1,39 @@
 class Solution {
     public List <Integer>  findDisappearedNumbers(int[] nums) {
+
+        int n = nums.length;
         
-        HashSet<Integer> set1= new HashSet<>();
-        HashSet<Integer> set2= new HashSet<>();
-        
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            set1.add(i+1);
-            set2.add(nums[i]);
-            
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
         }
-        HashSet<Integer> diff=new HashSet<>(set1);
-        diff.removeAll(set2);
         
-        List <Integer> result = new ArrayList<>(diff);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        
         return result;
+
+        
+        // HashSet<Integer> set1= new HashSet<>();
+        // HashSet<Integer> set2= new HashSet<>();
+        
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     set1.add(i+1);
+        //     set2.add(nums[i]);
+            
+        // }
+        // HashSet<Integer> diff=new HashSet<>(set1);
+        // diff.removeAll(set2);
+        
+        // List <Integer> result = new ArrayList<>(diff);
+        // return result;
 
         
     }
