c37f592ddaa31d7c1db86e064bf8b0d2e1abb0a2
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-04-14 14:16:42 +0530

    Time: 3 ms (87.79%), Space: 43.2 MB (76.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,41 @@
 class Solution {
     public boolean isValid(String s) {
-        int n=s.length();
-        if(n==1){
-            return false;
-        }
-        char[] arr=new char[s.length()];
-        int top=-1;
+        // int n=s.length();
+        // if(n==1){
+        //     return false;
+        // }
+        // char[] arr=new char[s.length()];
+        // int top=-1;
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(ch=='(' || ch=='{' || ch=='['){
+        //         top++;
+        //         arr[top]=ch;
+        //     }else{
+        //         if(top==-1) return false;
+        //         if(ch==')' && arr[top]!='(' || ch=='}' && arr[top]!='{' || ch==']' && arr[top]!='['){
+        //             return false;
+        //         }
+        //         top--;
+        //     }
+        // }        
+
+        // return top==-1;
+
+        Stack<Character> st= new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='(' || ch=='{' || ch=='['){
-                top++;
-                arr[top]=ch;
+                st.push(ch);
             }else{
-                if(top==-1) return false;
-                if(ch==')' && arr[top]!='(' || ch=='}' && arr[top]!='{' || ch==']' && arr[top]!='['){
+                if(st.isEmpty()){return false;}
+                if(ch==')' && st.peek()!='(' || ch=='}' && st.peek()!='{' || ch==']' && st.peek()!='['){
                     return false;
                 }
-                top--;
+                st.pop();
             }
         }
-        
 
-        
-
-        return top==-1;
+        return st.isEmpty();
     }
 }
