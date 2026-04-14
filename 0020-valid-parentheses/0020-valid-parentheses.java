567ca36e5cca753f033ece65c2d8b8974e946864
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-04-14 14:17:07 +0530

    Time: 1 ms (99.91%), Space: 42.8 MB (98.4%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,41 +1,41 @@
 class Solution {
     public boolean isValid(String s) {
-        // int n=s.length();
-        // if(n==1){
-        //     return false;
-        // }
-        // char[] arr=new char[s.length()];
-        // int top=-1;
-        // for(int i=0;i<s.length();i++){
-        //     char ch=s.charAt(i);
-        //     if(ch=='(' || ch=='{' || ch=='['){
-        //         top++;
-        //         arr[top]=ch;
-        //     }else{
-        //         if(top==-1) return false;
-        //         if(ch==')' && arr[top]!='(' || ch=='}' && arr[top]!='{' || ch==']' && arr[top]!='['){
-        //             return false;
-        //         }
-        //         top--;
-        //     }
-        // }        
-
-        // return top==-1;
-
-        Stack<Character> st= new Stack<>();
+        int n=s.length();
+        if(n==1){
+            return false;
+        }
+        char[] arr=new char[s.length()];
+        int top=-1;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='(' || ch=='{' || ch=='['){
-                st.push(ch);
+                top++;
+                arr[top]=ch;
             }else{
-                if(st.isEmpty()){return false;}
-                if(ch==')' && st.peek()!='(' || ch=='}' && st.peek()!='{' || ch==']' && st.peek()!='['){
+                if(top==-1) return false;
+                if(ch==')' && arr[top]!='(' || ch=='}' && arr[top]!='{' || ch==']' && arr[top]!='['){
                     return false;
                 }
-                st.pop();
+                top--;
             }
-        }
+        }        
 
-        return st.isEmpty();
+        return top==-1;
+
+        // Stack<Character> st= new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(ch=='(' || ch=='{' || ch=='['){
+        //         st.push(ch);
+        //     }else{
+        //         if(st.isEmpty()){return false;}
+        //         if(ch==')' && st.peek()!='(' || ch=='}' && st.peek()!='{' || ch==']' && st.peek()!='['){
+        //             return false;
+        //         }
+        //         st.pop();
+        //     }
+        // }
+
+        // return st.isEmpty();
     }
 }
