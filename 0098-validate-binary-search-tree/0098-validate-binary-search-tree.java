3a2a294963b11e3aba7ccba61fa3a216028543be
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-04-16 19:10:41 +0530

    Time: 0 ms (100%), Space: 45 MB (82.35%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,28 +15,31 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        List<Integer> ls=new ArrayList<>();
-        if(root==null) return true; 
-        // if(root.left < root && root.right < root) return false;
-
-        // isValidBST(root.left);
-        // isValidBST(root.right);
-
-        // return true;   
-
-        inorder(root,ls);
-
-        for(int i=1;i<ls.size();i++){
-            if(ls.get(i)<=ls.get(i-1)) return false;
-        }
-        return true;
+        return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
+    boolean valid(TreeNode root, long min,long max){
+        if(root==null) return true;
+        if(root.val <=min || root.val>=max) return false;
+        return valid(root.left,min,root.val) && valid(root.right,root.val,max);
+
+    }
+
+    // public boolean isValidBST(TreeNode root) {
+    //     List<Integer> ls=new ArrayList<>();
+    //     if(root==null) return true;  
+    //     inorder(root,ls);
+
+    //     for(int i=1;i<ls.size();i++){
+    //         if(ls.get(i)<=ls.get(i-1)) return false;
+    //     }
+    //     return true;
+    // }
     
-    void inorder(TreeNode root,List<Integer> ls){
-        if(root==null) return;
+    // void inorder(TreeNode root,List<Integer> ls){
+    //     if(root==null) return;
 
-        inorder(root.left,ls);
-        ls.add(root.val);
-        inorder(root.right,ls); 
-    }
+    //     inorder(root.left,ls);
+    //     ls.add(root.val);
+    //     inorder(root.right,ls); 
+    // }
 }
