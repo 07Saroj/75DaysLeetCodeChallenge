a9d6ee58f1c24f13720d41cb1e062082ba729cbd
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-04-28 22:48:32 +0530

    Time: 1 ms (99.84%), Space: 55.7 MB (32.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,9 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n=nums.length;
-        Arrays.sort(nums);
-        // Hashtable<Integer, Integer> ht= new Hashtable<>();
-        return nums[n/2];
+        int ele=nums[0];
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==ele){
+                count++;
+            }
+            else if(count==0){
+                ele=nums[i];
+            }else{
+                count--;
+            }
+        }
+        return ele;
+        // Arrays.sort(nums);
+        // // Hashtable<Integer, Integer> ht= new Hashtable<>();
+        // return nums[n/2];
         
     }
 }
