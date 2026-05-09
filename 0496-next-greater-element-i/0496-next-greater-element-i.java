8fd3d0c556edbf969c076bd1e18353f601d526b3
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-05-09 22:21:23 +0530

    Time: 4 ms (75.05%), Space: 45.3 MB (48.98%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,40 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int[] ans=new int[nums1.length];
-        Arrays.fill(ans,-1);
-        for(int i=0;i<nums1.length;i++){
+        Map<Integer, Integer> nextGreaterNum=new HashMap<>();
+        Deque<Integer> st= new ArrayDeque<>();
 
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    for(int k=j+1;k<nums2.length;k++){
-                        if(nums2[j]<nums2[k]){
-                            ans[i]=nums2[k];
-                            break;
-                        }
-                    }
-
-                }
+        for(int i=0;i<nums2.length;i++){
+            while(!st.isEmpty() && st.peek()<nums2[i]){
+                nextGreaterNum.put(st.pop(),nums2[i]);
             }
+            st.push(nums2[i]);
+        }
+
+        int[] ans=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i]= nextGreaterNum.getOrDefault(nums1[i],-1);
         }
 
         return ans;
+
+        // int[] ans=new int[nums1.length];
+        // Arrays.fill(ans,-1);
+        // for(int i=0;i<nums1.length;i++){
+
+        //     for(int j=0;j<nums2.length;j++){
+        //         if(nums1[i]==nums2[j]){
+        //             for(int k=j+1;k<nums2.length;k++){
+        //                 if(nums2[j]<nums2[k]){
+        //                     ans[i]=nums2[k];
+        //                     break;
+        //                 }
+        //             }
+
+        //         }
+        //     }
+        // }
+
+        // return ans;
     }
 }
