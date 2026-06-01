7cbb87e5b0851708b8acc51b1d74eccc6798aef8
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-02 00:45:54 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,13 @@
 class Solution {
     public int fib(int n) {
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        int[] F=new int[n+1];
+        F[0]=0;
+        F[1]=1;
+        for(int i=2;i<=n;i++){
+            F[i]=F[i-1]+F[i-2];
+        }
+
+        return F[n];
     }
 }
