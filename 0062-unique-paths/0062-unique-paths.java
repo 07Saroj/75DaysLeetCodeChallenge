8b93946c19efa3a1b2961da27a0193a0a1b58b5d
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-03 21:58:54 +0530

    Time: 0 ms (100%), Space: 42.3 MB (27.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -3,18 +3,35 @@ class Solution {
         if(m<1 || n<1) return 0;
         int[][] dp=new int[m][n];
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+            dp[i][0]=1;
+        }
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        return findPath(dp,0,0,m,n);
-        // return dp[m][n];
+
+        return dp[m-1][n-1];
+
+        // if(m<1 || n<1) return 0;
+        // int[][] dp=new int[m][n];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return findPath(dp,0,0,m,n);
+        
     }
 
-    int findPath(int[][] dp,int rs,int cs, int m,int n){
-        if(rs>= m|| cs>=n) return 0 ;
-        if(rs==m-1 || cs==n-1) return 1;
-        if(dp[rs][cs]!=-1) return dp[rs][cs];
-        return dp[rs][cs]=findPath(dp,rs+1,cs,m,n) + findPath(dp,rs,cs+1,m,n);
-    }
+    // int findPath(int[][] dp,int rs,int cs, int m,int n){
+    //     if(rs>= m|| cs>=n) return 0 ;
+    //     if(rs==m-1 || cs==n-1) return 1;
+    //     if(dp[rs][cs]!=-1) return dp[rs][cs];
+    //     return dp[rs][cs]=findPath(dp,rs+1,cs,m,n) + findPath(dp,rs,cs+1,m,n);
+    // }
 }
