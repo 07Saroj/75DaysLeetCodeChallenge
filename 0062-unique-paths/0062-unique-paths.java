dcdd86c318744a56dcefb1939d8269e23a2cd2a1
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-05 02:24:54 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.01%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,38 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        if(m<1 || n<1) return 0;
-        int[][] dp=new int[m][n];
-        // for(int i=0;i<m;i++){
-        //     dp[i][0]=1;
-        // }
-        // for(int j=0;j<n;j++){
-        //     dp[0][j]=1;
-        // }
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=1;
-                }else{
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
+        int[][] dp =new int[2][n];
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
+        }
+        dp[1][0]=1;  
                 
+        for(int i=1;i<=m-1;i++){
+            for(int j=1;j<n;j++){
+                dp[1][j]=dp[0][j]+dp[1][j-1];
+                
+            }
+            for (int j = 1; j < n; j++) {
+                dp[0][j] = dp[1][j];
             }
         }
 
-        return dp[m-1][n-1];
+        return m == 1 ? dp[0][n - 1] : dp[1][n - 1];
 
-        // if(m<1 || n<1) return 0;
+
+        // int[][] dp=new int[m][n];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 || j==0){
+        //             dp[i][j]=1;
+        //         }else{
+        //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        //         }
+                
+        //     }
+        // } 
+        // return dp[m-1][n-1];
+
+
         // int[][] dp=new int[m][n];
         // for(int i=0;i<m;i++){
         //     for(int j=0;j<n;j++){
