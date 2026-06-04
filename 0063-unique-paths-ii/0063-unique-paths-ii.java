31aea42a953cbf16a45fe789b3107e8995522a41
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-05 00:58:41 +0530

    Time: 0 ms (100%), Space: 43.7 MB (10.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -11,8 +11,8 @@ class Solution {
                     dp[i][j]=0;
                     continue;
                 }else if(i==0 && j==0) dp[0][0]=1;
-                else if(i==0) dp[i][j]=dp[i][j-1];
-                else if(j==0)  dp[i][j]=dp[i-1][j];
+                else if(i==0) dp[0][j]=dp[0][j-1];
+                else if(j==0)  dp[i][0]=dp[i-1][0];
                 else{
                     dp[i][j]=dp[i][j-1]+dp[i-1][j];
                 }
