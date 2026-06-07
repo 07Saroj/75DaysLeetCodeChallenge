fd7cb25157f17073e8c2b59f91957ccf7fe406ba
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-07 22:29:07 +0530

    Time: 376 ms (10.66%), Space: 105.5 MB (8.63%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -4,15 +4,19 @@ class Solution {
         if(idx==nums.size()){ // base case 2: we reach at the end and we do not get the target
             return Integer.MIN_VALUE;
         }
+        long peak=Integer.MIN_VALUE;
         if(dp[idx][target]!=-1) return dp[idx][target];
         long skip=longestSubsequence(idx+1,dp,nums,target);
         if(target-nums.get(idx)<0) return dp[idx][target]=skip;
         // *******//
-        long peak=Integer.MIN_VALUE;
-        long res=longestSubsequence(idx+1,dp,nums,target-nums.get(idx));
-        if(res!=Integer.MIN_VALUE){
-            peak=1+res;
+        else{
+            
+            long res=longestSubsequence(idx+1,dp,nums,target-nums.get(idx));
+            if(res!=Integer.MIN_VALUE){
+                peak=1+res;
+            }
         }
+        
         
         return dp[idx][target]=Math.max(skip,peak);
     }
