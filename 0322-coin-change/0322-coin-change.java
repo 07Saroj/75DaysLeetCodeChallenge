e90e52b03f4e9e28b4a2941f6695369cd227e3ef
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-07 20:26:40 +0530

    Time: 39 ms (8.59%), Space: 47.9 MB (5.15%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    private long  minCoins(int idx,int[][]dp,int[]coins,int amount){
+        if(idx==coins.length ){
+            if(amount==0) return 0;//no more coins needed;
+            else return Integer.MAX_VALUE;// not avalid combination
+        }
+
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        long skip=minCoins(idx+1,dp,coins,amount);
+        if(amount-coins[idx]<0)return dp[idx][amount]=(int)skip;
+        long take=1+minCoins(idx,dp,coins,amount-coins[idx]);
+
+        return dp[idx][amount]=(int)Math.min(skip,take);
+    }
+    public int coinChange(int[] coins, int amount) {
+        if(amount==0) return 0;
+        int[][] dp=new int[coins.length][amount+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
+        }
+        int ans=(int)minCoins(0,dp,coins,amount);
+        return (ans==Integer.MAX_VALUE)? -1 : ans;
+    }
+}
