529f1cfe32fdaf35a65cb6e368b49c916da04965
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-08 21:25:43 +0530

    Time: 87 ms (25.92%), Space: 75.7 MB (16.06%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -21,14 +21,36 @@ class Solution {
         if(sum%2!=0) return false ;
         int target=sum/2;
         int[][] dp=new int[nums.length][target+1];
+        // boolean ans=false;
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
+               
+                boolean ans=false;
+                boolean skip=(i>0)?(dp[i-1][j]==1): (j==0);
+                if(j-nums[i]<0) ans=skip;
+                else{
+                    boolean pick=(i>0)?(dp[i-1][j-nums[i]]==1): (j==0);
+                    ans=skip || pick;
+                    
+                }
+                dp[i][j]=(ans)? 1:0;
             }
         }
+        return (dp[nums.length-1][target]==1);
+        // int sum=0;
+        // for(int ele: nums) sum+=ele;
+        // if(sum%2!=0) return false ;
+        // int target=sum/2;
+        // int[][] dp=new int[nums.length][target+1];
+       
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
         
         
-        return subsetSum(0,dp,nums,target);
+        // return subsetSum(0,dp,nums,target);
         
     }
 }
