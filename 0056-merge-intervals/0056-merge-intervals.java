51478386b55609e1c59cfafaf9771083f50e04c3
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-10 20:46:35 +0530

    Time: 8 ms (90.65%), Space: 48.8 MB (82.75%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,13 +5,10 @@ class Solution {
         if (intervals.length <= 1) {
             return intervals;
         }
-
-        // 1. Crucial Step: Sort intervals by their start time
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> ls = new ArrayList<>();
-        
-        // Start by pushing the first interval into the list
+
         int[] currentInterval = intervals[0];
         ls.add(currentInterval);
 
@@ -20,17 +17,15 @@ class Solution {
             int nextStart = interval[0];
             int nextEnd = interval[1];
 
-            // 2. If the next interval overlaps with the current one, merge them
             if (currentEnd >= nextStart) {
                 currentInterval[1] = Math.max(currentEnd, nextEnd);
             } else {
-                // 3. If they don't overlap, move your pointer to the new interval
                 currentInterval = interval;
                 ls.add(currentInterval);
             }
         }
 
-        // Convert List back to 2D Array
+        
         return ls.toArray(new int[ls.size()][]);
     }
 }
