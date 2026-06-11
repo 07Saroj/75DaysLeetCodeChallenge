f597926f33a9696ec9346531b96e7817d6d1111d
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-11 23:47:16 +0530

    Time: 20 ms (78.42%), Space: 53.9 MB (67.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,40 +1,58 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        //Space optimized
         int m=text1.length();
         int n=text2.length();
         
-        int[][] dp=new int[2][n+1];
+        int[][] dp=new int[m+1][n+1];
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[1][j]=1+dp[0][j-1];
+                    dp[i][j]=1+dp[i-1][j-1];
                 }    
                 else{
-                    dp[1][j]=Math.max(dp[0][j],dp[1][j-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            for(int j=0;j<=n;j++){
-                dp[0][j]=dp[1][j];
-            }
         }
+        //print string
+        // String ans="";
+        // int i=m;
+        // int j=n;
+        // while(i>0 && j>0){
+        //     if(text1.charAt(i-1)==text2.charAt(j-1)) ans+=text1.charAt(i-1);
+        //     if(dp[i-1][j]>=dp[i][j-1]){
+        //         i--;
+        //     }else{
+        //         j--;
+        //     }
+           
+        // }
+            
+        // System.out.println(new StringBuilder(ans).reverse().toString());       
 
-        return dp[1][n];
+
+        return dp[m][n];
+        
+        //Space optimized
         // int m=text1.length();
         // int n=text2.length();
         
-        // int[][] dp=new int[m+1][n+1];
+        // int[][] dp=new int[2][n+1];
         // for(int i=1;i<=m;i++){
         //     for(int j=1;j<=n;j++){
         //         if(text1.charAt(i-1)==text2.charAt(j-1)){
-        //             dp[i][j]=1+dp[i-1][j-1];
+        //             dp[1][j]=1+dp[0][j-1];
         //         }    
         //         else{
-        //             dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+        //             dp[1][j]=Math.max(dp[0][j],dp[1][j-1]);
         //         }
+        //     }
+        //     for(int j=0;j<=n;j++){
+        //         dp[0][j]=dp[1][j];
         //     }
         // }
 
-        // return dp[m][n];
+        // return dp[1][n];
+        
     }
 }
