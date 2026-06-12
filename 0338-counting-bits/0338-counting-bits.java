16163f0e04210c1aaaf189ed493612ec2297361e
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-12 15:15:11 +0530

    Time: 10 ms (8.27%), Space: 48.8 MB (30.01%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int bits(int n){
+        if(n==0)return 0;
+       int res=0;
+        while(n>0){
+            if((n%2)==1){
+                res++;
+            }
+            n/=2;
+        }
+        return res;
+    }
+    public int[] countBits(int n) {
+        int[] ans=new int[n+1];
+        for(int i=0;i<=n;i++){
+            ans[i]=bits(i);
+        }
+        return ans;
+    }
+}
