73d2829f46e7731e9d505730891987a146607267
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-06-13 12:34:52 +0530

    Time: 0 ms (100%), Space: 47.5 MB (19.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,21 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
         int n=nums.length;
-        int pt=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=pt){
-               return pt;
-            }
-            pt++;
+        int idealSum=n*(n+1)/2;
+        int realSum=0;
+        for(int i=0 ;i<n; i++){
+            realSum+=nums[i];
         }
-        return n;
+        return idealSum-realSum;
+        // Arrays.sort(nums);
+        // int n=nums.length;
+        // int pt=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=pt){
+        //        return pt;
+        //     }
+        //     pt++;
+        // }
+        // return n;
     }
 }
