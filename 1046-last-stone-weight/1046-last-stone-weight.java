7c482e44f2b166b7720260aa1f9e126297255bb3
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-08-28 19:49:57 +0530

    Time: 1 ms (99.65%), Space: 43.1 MB (15.11%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,55 +1,21 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Comparator.reverseOrder());
 
-        PriorityQueue<Integer> maxHeap= new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int s : stones){
-            maxHeap.add(s);
+        for(int ele: stones){
+            pq.add(ele);
         }
 
-        while(maxHeap.size()>1){
-            int s1=maxHeap.poll();
-            int s2=maxHeap.poll();
-
-            if(s1!=s2){
-                maxHeap.add(s1-s2);
+        while(pq.size()>1){
+            int x=pq.poll();
+            int y=pq.poll();
+            if(x!=y){
+                pq.add(x-y);
             }
         }
-
-        if(maxHeap.size()==1){
-            return maxHeap.peek();
+        if(pq.size()==1){
+            return pq.peek();
         }
         return 0;
-        
-    //    int n=stones.length;
-    //    if(n==1){
-    //     return stones[0];
-    //    }
-    //     Arrays.sort(stones);
-
-    //     for(int i=n-1;i>=0;i--){
-    //         int j=i-1;
-    //         if(j>=0){
-    //             if(stones[i]==stones[j] && stones[i]>0 && stones[j]>0){
-    //             stones[i]=stones[j]=-1;
-    //             }    
-    //             else if(stones[i]!=stones[j] && stones[j]>0){
-    //                 stones[i]-=stones[j];
-    //                 stones[j]=-1;
-    //             } 
-    //         }
-               
-
-    //     }
-
-    //     for(int stone: stones){
-    //         if(stone>0){
-    //             return stone;
-    //         }
-                
-            
-    //     }
-    //     return 0;
-        
     }
 }
