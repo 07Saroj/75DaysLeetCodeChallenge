96b2b6baa30030ad61e64ecb15107868aa587632
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-08-29 16:49:11 +0530

    Time: 42 ms (10.42%), Space: 48.3 MB (36.56%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,40 +1,46 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] freqs = new int[26];
-        PriorityQueue<Integer> maxHeap= new PriorityQueue<>(Collections.reverseOrder()) ;
-        for(char task: tasks){
-            int idx=task-'A';
-            freqs[idx]+=1;
-        }
-
-        for(int f : freqs){
-            if(f>0)maxHeap.add(f);
-        }
-
-        int time=0;
-        while(!maxHeap.isEmpty()){
-            int cycle=n+1;
-            int count=0;
-
-            List<Integer> temp= new ArrayList<>();
-            while(cycle >0 && !maxHeap.isEmpty()){
-                temp.add(maxHeap.poll()-1);
-                cycle--;
-                count++;
-            }
-
-            for(int t: temp){
-                if(t>0) maxHeap.add(t);
-            }
-
-            if(maxHeap.isEmpty()){
-                time+=count;
+        //1.count the frequencies of the tasks
+        Map<Character,Integer> mp= new HashMap<>();
+        for(char ch: tasks){
+            if(mp.containsKey(ch)){
+                mp.put(ch,mp.get(ch)+1);
             }else{
-                time+=n+1;
+                mp.put(ch,1);
             }
         }
-        return time;
+        //Add freqs in the pq
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Comparator.reverseOrder());
+
+        for(char key: mp.keySet()){
+            pq.add(mp.get(key));
+        }
+        int totalTime=0;
+        while(!pq.isEmpty()){
+            List<Integer> temp= new ArrayList<>();
+
+            for(int i=0;i<=n;i++){
+                if(!pq.isEmpty()){
+                    int freq=pq.poll();
+
+                    if(freq>1){
+                        temp.add(freq-1);// add rest of the freq to temp
+                    }
+                }
+                totalTime++;
+
+                if(pq.isEmpty() && temp.isEmpty()){
+                    break;//all tasks are done
+                }
+            }
+
+            for(int freq: temp){
+                pq.add(freq);
+            }
 
 
+        }
+
+        return totalTime;
     }
 }
