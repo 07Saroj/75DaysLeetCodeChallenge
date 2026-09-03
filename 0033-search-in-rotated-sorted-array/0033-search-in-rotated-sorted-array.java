e9ac75dcc1b33f6342249217a7456f25a0dc55c9
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-09-03 08:38:49 +0530

    Time: 0 ms (100%), Space: 43.7 MB (82.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0;
-        int r=nums.length-1;
+        int l = 0;
+        int r = nums.length - 1;
 
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){
+        while (l <= r) {
+            // Calculate mid to avoid potential integer overflow
+            int mid = l + (r - l) / 2;
+
+            // Target found, return index
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[mid]<=nums[r] ){
-                if(nums[mid]<target && target<=nums[r]){
-                    l=mid+1;
-                }else{
-                    r=mid-1;
+            } 
+            // Check if the RIGHT half is strictly sorted
+            else if (nums[mid] <= nums[r]) {
+                // If target lies within the sorted right half, search right
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } 
+                // Otherwise, search in the left half
+                else {
+                    r = mid - 1;
                 }
-                
-            }else {
-                if(nums[l]<=target && nums[mid]>target){
-                    r=mid-1;
-                }else{
-                    l=mid+1;
+            } 
+            // Otherwise, the LEFT half must be sorted
+            else {
+                // If target lies within the sorted left half, search left
+                if (nums[l] <= target && nums[mid] > target) {
+                    r = mid - 1;
+                } 
+                // Otherwise, search in the right half
+                else {
+                    l = mid + 1;
                 }
             }
         }
+
+        // Target was not found in the array
         return -1;
     }
 }
