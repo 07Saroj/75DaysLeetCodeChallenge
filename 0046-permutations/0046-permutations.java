01c165555884fa4e26aa6a109896194a23a2ef90
Saroj Chaudhuri <sarojchaudhuri203@gmail.com>
2026-09-11 23:44:52 +0530

    Time: 0 ms (100%), Space: 45.7 MB (28.98%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,30 @@
 class Solution {
-    static void helper(int[] nums,List<Integer> ls,boolean[]isVisited,List<List<Integer>> ans){
-        if(ls.size()==nums.length){
-            ans.add(new ArrayList(ls));//we have to create a shallow copy of the list before storing it. Passing ls directly would result in adding empty lists due to reference sharing.
+    static void helper(int[] nums,int idx,List<List<Integer>> ans){
+        if(idx==nums.length-1){//base case
+            List<Integer> ls = new ArrayList<>();
+            for(int num : nums){
+                ls.add(num);
+            }
+            ans.add(ls);
             return;
         }
         
-        for(int i=0;i<nums.length;i++){
-            if(!isVisited[i]){
-                ls.add(nums[i]);
-                isVisited[i]=true;
-                helper(nums,ls,isVisited,ans);
-                isVisited[i]=false;
-                ls.remove(ls.size()-1);
-            }
+        for(int i=idx;i<nums.length;i++){
+            swap(i,idx,nums);
+            helper(nums,idx+1,ans);
+            swap(i,idx,nums);
         }
 
+    }
+    private static void swap(int idx1,int idx2,int[]nums){
+        int temp=nums[idx1];
+        nums[idx1]=nums[idx2];
+        nums[idx2]=temp;
     }
     public List<List<Integer>> permute(int[] nums) {
 
         List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> ls= new ArrayList<>();
-        boolean[]isVisited=new boolean[nums.length];
-        helper(nums,ls,isVisited,ans);
+        helper(nums,0,ans);
         return ans;
     }
 }
